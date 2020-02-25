435e3a9e71a1128b7a4399e45181f9e057413e92
Matt Oestreich <matthewpoestreich@gmail.com>
2020-02-24 21:36:32 -0600

    init

--- index.js
+++ index.js
@@ -9,12 +9,15 @@ const child = execFile('node', ['runOnFileChange.js']);
 
 child.stdout.on('data', data => {
     // Only here to let us know it's working...
-    console.log(`Data Received: '${data.toString()}'`); 
+    console.log(`Data Received: '${data.toString().trim()}'`); 
 });
 
 let count = 0;
 const interval = setInterval(() => {
-    if (count === 10) clearInterval(interval);
+    if (count === 9) {
+        clearInterval(interval);
+        child.kill();
+    }
     fs.appendFile('./log.txt', 'Added data', err => {
         if (err) console.log(err);
     });
